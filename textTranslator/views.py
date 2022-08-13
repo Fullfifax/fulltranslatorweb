9ad4ce9486bca7b255ef174c69aa889b6d706e9c
Fullfifax <fullfifax1245@gmail.com>
2022-08-13 12:13:00 +0300

    adding translate view method

--- textTranslator/views.py
+++ textTranslator/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from googletrans import Translator
 
 # Create your views here.
+
+def translate_text(request):
+    if request.method == "POST":
+        lang = request.POST.get("lang", None)
+        txt = request.POST.get("txt", None)
+
+        translator = Translator()
+        translation = translator.translate(txt, dest=lang)
+
+        return render(request, 'translate.html', {"result": translation.text})
+
+    return render(request, 'translate.html')
